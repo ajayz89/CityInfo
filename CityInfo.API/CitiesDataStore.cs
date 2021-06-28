0f75b8ba416ea81080e09b640fa620d5e22c5f9f
Ajay Singh <ajay.singh@elfo.net>
2021-06-28 18:03:03 +0200

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,55 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>() {
+              new CityDto(){
+              Id = 1,
+              Name = "New york City",
+              Description = "City That Never Sleeps",
+              PointOfInterests = new List<PointOfInterestDto>() {
+              new PointOfInterestDto(){
+                  Id = 1,
+                  Name = "Statue Of Liberty",
+                  Description = "Rapresents Freedom"
+              },
+              new PointOfInterestDto(){
+                  Id = 2,
+                  Name = "Central Park",
+                  Description = "Park.."
+              }
+              }
+              },
+              new CityDto(){
+              Id = 2,
+              Name = "Rome",
+              Description = "Impressive culture",
+               PointOfInterests = new List<PointOfInterestDto>() {
+              new PointOfInterestDto(){
+                  Id = 1,
+                  Name = "Colosseum",
+                  Description = "Battles.."
+              },
+              new PointOfInterestDto(){
+                  Id = 2,
+                  Name = "Pantheon",
+                  Description = "Roman Church.."
+              }
+              }
+              }
+            };
+        }
+    }
+}
+
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            return Ok (CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleCity(int id ) {
+            var requestedCity = CitiesDataStore.Current.Cities.FirstOrDefault(x=> x.Id == id);
+            if (requestedCity == null)
+                return NotFound();
+            else
+                return Ok(requestedCity);
+
+        }
+    }
+}
